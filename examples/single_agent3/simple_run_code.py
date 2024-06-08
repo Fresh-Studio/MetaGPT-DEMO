ed6247f54f8486d589d49dcb42313f4c7cd8d601
ustc-yxw <yusingh@mail.ustc.edu.cn>
2024-06-08 20:22:21 +0800

    修复runnable coder执行python代码失败的问题

--- examples/single_agent3/simple_run_code.py
+++ examples/single_agent3/simple_run_code.py
@@ -6,8 +6,8 @@ class SimpleRunCode(Action):
     name: str = "SimpleRunCode"
 
     async def run(self, code_text: str):
-        logger.info(f"{code_text=}")
-        result = subprocess.run(["python3", "-c", code_text], capture_output=True, text=True)
+        # logger.info(f"{code_text=}")
+        result = subprocess.run(["python", "-c", code_text], capture_output=True, text=True)
         code_result = result.stdout
         logger.info(f"{code_result=}")
         return code_result
