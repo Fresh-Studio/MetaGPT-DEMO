12c5ce2d95df5f286b967f6875da8c9ad9182f7c
ustc-yxw <yusingh@mail.ustc.edu.cn>
2024-07-14 21:14:20 +0800

    init homework of chapter 3

---
+++ examples/chapter3_homework/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2024/7/14 21:13
+@Author  : yusingh
+@File    : main.py
+Homework of Chapter 3(https://www.feishu.cn/community/article/wiki?id=7382188504203132929)
+"""
