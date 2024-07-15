95a76c2aeb06547a7ef31c7e4a83a3602eae550a
yusingh <yusingh@mail.ustc.edu.cn>
2024-07-15 18:59:54 +0800

    add Printer

--- examples/chapter3_homework/main.py
+++ examples/chapter3_homework/main.py
@@ -6,3 +6,31 @@
 @File    : main.py
 Homework of Chapter 3(https://www.feishu.cn/community/article/wiki?id=7382188504203132929)
 """
+from metagpt.actions import Action
+from metagpt.roles import Role
+
+class Print(Action):
+    """Action class for printing some informations
+
+    Args:
+        name: name of action
+        info: info to print
+    """
+
+    name: str = "Print"
+    info: str = ""
+
+    async def run(self):
+        print("打印{info}")
+
+class Printer(Role):
+    """Role of printer
+    """
+
+    name: str = "Printer"
+
+    def __init__(self, **kwargs):
+        super.__init__(kwargs)
+        self.set_actions([Print(info="1"), Print(info="2"), Print(info="3")])
+
+    def 
